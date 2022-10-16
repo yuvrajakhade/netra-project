a6162d3284a66714312f09d6208bdab30f76086f
yuvraj <yuvraj.akhade399.com>
2022-10-16 12:00:21 +0530

    Added register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   hide = true;
+
+  registerForm = new FormGroup({
+    f_name: new FormControl('', [Validators.required]),
+    l_name: new FormControl(''),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,63}$'),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$'
+      ),
+    ]),
+  });
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
 }
